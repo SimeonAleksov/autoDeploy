40ac1463a751c2c2e96843db9ded3c1a37365c6f
Simeon <SimeonAleksov@users.noreply.github.com>
2020-01-05 02:10:03 +0100

    minor change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 from threading import Thread
+import time
+import paramiko
 sys.path.append("c:\\Users\\Samoil\\Desktop\\auto\\cmd")
 sys.path.append('c:\\Users\\Samoil\\Desktop\\auto\\helpers')
 sys.path.append('c:\\Users\\Samoil\\Desktop\\auto\\connections')
@@ -8,16 +10,14 @@ from ssh_client import SSH
 from loading import *
 from user_input import *
 from colors import bcolors
-import time
-import paramiko
+
 if __name__ == "__main__":
     welcome_banner()
     host_name, username, pw, gitRepo = get_user_info()
-    # client = SSH(host_name, username, pw, paramiko.SSHClient())
-    client = SSH('83.212.126.194','user', 'root123', paramiko.SSHClient())
+    client = SSH(host_name, username, pw, paramiko.SSHClient())
     threads = []
-    # t = Thread(target=loading())
-    # threads.append(t)
+    t = Thread(target=loading())
+    threads.append(t)
     t1 = Thread(target=client.connect())
     threads.append(t1)
     for x in threads:
@@ -27,24 +27,23 @@ if __name__ == "__main__":
     if client.is_alive():
         web_type = check_web_type()
         if check_continue():
-            # installing()
+            installing()
             linux_cmd = Linux(gitRepo)
             commands = []
             commands.append(linux_cmd.update_rep())
+            commands.append(linux_cmd.install_apache())
+            commands.append(linux_cmd.clear_apache_html())
             clone, copy = linux_cmd.handle_repos()
             commands.append(clone)
             commands.append(copy)
             commands.append(linux_cmd.reload_apache())
-            print(commands)
             sys.exit()
             for cmd in commands:
                 status_code, output = client.execute(cmd)
                 time.sleep(5)
                 if status_code == 0:
-                    print(output.decode('utf-8'))
-                    print('Success')
+                    print(cmd)
                 else:
-                    print(output.decode('utf-8'))
                     print(f"Error {status_code}")
-
-            print(f"{bcolors.OKGREEN} Installing website successful. You can check it here ")
+            _, output = client.execute(linux_cmd.get_server_ip())
+            print(f"{bcolors.OKGREEN} Installing website successful. You can check it here {output.decode('utf-8')}")
